e6902cf1083d9a78955867b22a37903e9623d303
JuanLopezAranzazu <juanlopezaranzazu@gmail.com>
2025-12-06 11:41:39 -0500

    Se ajustaron los nombres de carpeta de los codelabs

--- object-detection-ssd/yolo_image.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model_yolo = YOLO("yolov8n.pt")
-results = model_yolo("perros.jpg")
-results[0].show()
--- object-detection-yolo/utils/model_loader.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-def load_model(model_path="yolov8n.pt"):
-    """Carga y devuelve un modelo YOLOv8."""
-    return YOLO(model_path)
